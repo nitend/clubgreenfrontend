c33210c9882b280bfb6893decb068103ce2c453a
Sven <sven@d-nite.de>
2020-06-11 22:14:36 +0200

    error on server not reachable

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect} from 'react'
 import { Routes } from './pages/pageframe/Routes'
 import { setAccessToken } from './accessToken';
+import { ErrorMessage } from './pages/pageframe/global/Messages/ErrorMessage';
+
 
 interface Props {
 }
@@ -13,6 +15,7 @@ type auth = {
 export const App: React.FC<Props> = () => {
 
     const [isloading, setLoading] = useState(true);
+    const [failed, isFailed] = useState();
 
     useEffect(() => {
         fetch ("http://localhost:4000/refresh_token", {
@@ -23,9 +26,17 @@ export const App: React.FC<Props> = () => {
             setAccessToken(accessToken)
             setLoading(false)
             }
+        ).catch( error => {
+            isFailed(error)
+        }     
         );
-        
     }, [])
+
+    if(failed){
+        return (         
+            <ErrorMessage error={failed}  show={true}/>   
+        );
+    }
 
     if(isloading){
         return (         
